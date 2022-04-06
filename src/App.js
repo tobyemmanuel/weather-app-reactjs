c002ebd2582c28b2cfdb7a71249450ff7402004d
tobyemmanuel <tobyemmanuel7@gmail.com>
2022-04-06 17:26:33 +0100

    Passed Data Values to the Top Left Section

--- src/App.js
+++ src/App.js
@@ -1,52 +1,82 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Topleft from './components/Topleft';
 import Topright from './components/Topright';
 import Bottom from './components/Bottom';
 
-
 const api = {
-  API_URL:"http://api.weatherapi.com/v1/forecast.json",
-  API_KEY : "b74ed1d22a254a1f90a50035221103",
-  API_DAY_DATA : "5"
+  API_URL: "http://api.weatherapi.com/v1/forecast.json",
+  API_KEY: "b74ed1d22a254a1f90a50035221103",
+  API_DAY_DATA: "5"
 }
 
 function App() {
   const [dLocation, setDLocation] = useState('London');
+  const [locationValues, setLocationValues] = useState({});
+  const [forecastValues, setForecastValues] = useState({});
+  const [currentValues, setCurrentValues] = useState({});
   const [defaultLocation, setDefault] = useState(true);
-  const [WeatherValues, setWeatherValues] = useState({});
-
+  const monthText = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const pad = (num) => {
+    var numstr = num.toString();
+    if (num < 10) numstr = "0" + numstr;
+    return numstr;
+  }
 
   const onChangeLocation = (event) => {
+    setDefault(false);
     var newLocation = event.target.value;
     setDLocation(newLocation);
-   // 
+    console.log(defaultLocation)
   }
 
   const submittedLocation = (event) => {
-    if(event.key === 'Enter'){
+    if (event.key === 'Enter') {
       event.preventDefault();
-      if(dLocation.length > 2){  fetchData();  }else{
+      if (dLocation.length > 2) {
+        // setLocationValues({});
+        fetchData();
+      } else {
         alert('Please insert a valid location.')
       }
-     
+
     }
   }
 
   const fetchData = () => {
+    setDefault(false);
     fetch(`${api.API_URL}?key=${api.API_KEY}&q=${dLocation}&days=${api.API_DAY_DATA}`)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
+      .then(response => response.json())
+      .then(data => {
+        if (data.error !== undefined && data.error.code !== undefined) {
+          alert(data.error.message);
+          setDLocation('London');
+        } else {
+          setLocationValues({
+            status: 1,
+            ...data.location
+          });
+          setCurrentValues({
+            status: 1,
+            ...data.current
+          });
+          setForecastValues({
+            status: 1,
+            ...data.forecast
+          });
+        }
+      });
+
+
+  }
 
   useEffect(() => {
-        if(defaultLocation == true){
-           fetchData();
-           //console.log('Yes')
-        }
-        setDefault(false);
-  })
+    fetchData();
+    console.log(locationValues)
+  }, [locationValues.status, defaultLocation])
+
+ 
 
   return (
     <div className="weather-app-body">
@@ -57,10 +87,18 @@ function App() {
           />
     <div className="container pt-4 px-4 ">
         <div className="row main-container px-md-5 px-sm-2 py-md-4 py-sm-2">
-           <Topleft/>
-            <Topright/>
+           <Topleft
+           locationData = {locationValues}
+           padding = {pad}
+           monthText = {monthText}
+           />
+            <Topright
+            currentData = {currentValues}
+            />
             <hr className="hr"/>
-           <Bottom/>
+           <Bottom
+           forecastData = {forecastValues}
+           />
         </div>
     </div>
     </div>
--- src/components/Topleft.js
+++ src/components/Topleft.js
@@ -1,26 +1,41 @@
 import React from 'react'
 
-export default function Topleft() {
+export default function Topleft(props) {
+    const {
+        locationData,
+        padding,
+        monthText
+    } = props
+
+    const dateTime = new Date(locationData.localtime_epoch * 1000);
+    var hours = 0;
+    var amOrpm = 'am';
+
+    hours = dateTime.getHours();
+    amOrpm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
   return (
     <div className="col-md-6  ">
     <div className="row">
         <div className="col-md-7 col-sm-12 pt-4">
-           <h1 className="city">london</h1>
-           <h2 className="country">United Kingdom</h2>
+           <h1 className="city">{locationData.name}</h1>
+           <h2 className="country">{locationData.country}</h2>
         </div>
         <div className="col-md-5 col-sm-12">
             <div className="time-date align-content-center ">
                 <div className="time d-flex justify-content-start p-0">
                     <span className="time-icon align-self-center"><img className="time-icon" src="assets/images/time-icon.png" alt=""/></span>
-                    <span className="hour p-0">09 </span>
+                    <span className="hour p-0">{padding(hours)}</span>
                     <div className="hour-period d-flex flex-column justify-content-around align-self-center">
-                        <span className="mins">45</span>
-                        <span className="period">am</span>
+                        <span className="mins">{padding(dateTime.getMinutes())}</span>
+                        <span className="period">{amOrpm}</span>
                     </div>               
                 </div>
                 <div className="date d-flex justify-content-start">
                     <span className="date-icon align-self-center"><img className="date-icon" src="assets/images/date-icon.png" alt=""/></span>
-                    <span className="date-text align-self-center">Mar. 24</span>
+                    <span className="date-text align-self-center">{monthText[dateTime.getMonth()]}. {padding(dateTime.getDate())}</span>
                 </div>
             </div>
         </div>
